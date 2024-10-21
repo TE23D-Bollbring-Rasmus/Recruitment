0554a197e72be2339f03e411e912db081412d28a
Nils Rasmus Mattsson Bollbring <rasmus.mattssonbollbring@elev.ga.ntig.se>
2024-10-21 09:46:11 +0200

    first commit

---
+++ Reucruitment/Program.cs
@@ -0,0 +1,54 @@
+﻿﻿List<string> möjligaRecruits = [
+"Cynthia Mccoy",
+"Milford Carroll",
+"Cedric Bond",
+"Isabelle Guzman",
+"Daryl Marquez",
+"Cecilia Pollard",
+"Jackie Zuniga",
+"Gilbert Villarreal",
+"Mayra Wolfe",
+"Gerry Glenn",
+  ];
+List<string> nuvarandearbetare = [];
+
+while (nuvarandearbetare.Count < 2)
+{
+
+  Console.WriteLine("-- Potential recruits --");
+  for (int i = 0; i < möjligaRecruits.Count; i++)
+  {
+    Console.WriteLine($"{i} {möjligaRecruits [i]}");
+  }
+  Console.WriteLine("\n-- Current recruits --");
+  for (int i = 0; i < nuvarandearbetare.Count; i++)
+  {
+    Console.WriteLine($"{nuvarandearbetare [i]}");
+  }
+
+  Console.WriteLine("\nChoose a recruit:");
+  int rNum = 0;
+  bool couldConvert = false;
+  while (couldConvert == false || rNum < 0 || rNum >= möjligaRecruits.Count)
+  {
+    string nText = Console.ReadLine();
+    couldConvert = int.TryParse(nText, out rNum);
+
+    if (couldConvert == false)
+    {
+      Console.WriteLine("That's not a number!");
+    }
+    else if (rNum < 0 || rNum >= möjligaRecruits.Count)
+    {
+      Console.WriteLine("That's not a valid index!");
+    }
+  }
+
+  string recruit = möjligaRecruits[rNum];
+  nuvarandearbetare.Add(recruit);
+  möjligaRecruits.RemoveAt(rNum);
+}
+
+Console.WriteLine($"You recruited a total of {nuvarandearbetare.Count} soldiers.");
+
+Console.ReadLine();
